25459c0e936f8bd1083116efa9de98e12303cd1f
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-06-03 13:22:42 -0700

    Added State Context file to add/remove items from shopping cart.

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -4,10 +4,13 @@ import { client, urlFor } from '../../lib/client';
 import { AiOutlineMinus, AiOutlinePlus, AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import { Product } from '../../components';
 
+import { useStateContext } from '../../context/StateContext'
+
 const ProductDetails = ({ product, products }) => {
     const { image, name, details, price } = product
 
     const [index, setIndex] = useState(0);
+    const { decQty, incQty, qty, onAdd } = useStateContext();
 
     return (
         <div>
@@ -50,19 +53,19 @@ const ProductDetails = ({ product, products }) => {
                         <h3>Quantity:</h3>
                         <p className='quantity-desc'>
                             <span className='minus'
-                            onClick=""><AiOutlineMinus />
+                            onClick={decQty}><AiOutlineMinus />
                             </span>
                             <span className='num'
-                            onClick="">0</span>
+                            onClick="">{qty}</span>
                             <span className='plus'
-                            onClick=""><AiOutlinePlus />
+                            onClick={incQty}><AiOutlinePlus />
                             </span>
                         </p>
                     </div>
                     <div className='buttons'>
                         <button type='button'
                         className='add-to-cart'
-                        onClick="">Add to Cart
+                        onClick={() => onAdd(product, qty)}>Add to Cart
                         </button>
                         <button type='button'
                         className='buy-now'
