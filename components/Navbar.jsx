217646c3cad0756e285b98e74b6cd602f1a0b9bf
Chris Jimenez <chrisjimenez8118@gmail.com>
2022-06-06 15:39:06 -0700

    Added toggle feature to shopping cart to add or delete items.

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href="/">Desktop</Link>
+        <Link href="/">Home</Link>
       </p>
 
       <button type='button' className='cart-icon' onClick={() => 
